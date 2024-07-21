84ae508029cc1362c1109bada9b7fafa1cad2e68
Lazbert <lambert.leung.1@gmail.com>
2024-07-21 23:49:55 +0800

    catch P2002 duplicate error

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
@@ -15,23 +19,37 @@ export class AuthService {
     );
 
     // save new user in db
-    const user =
-      await this.prismaService.user.create({
-        data: {
-          email: authDto.email,
-          hash,
-        },
+    try {
+      const user =
+        await this.prismaService.user.create({
+          data: {
+            email: authDto.email,
+            hash,
+          },
 
-        // select: {
-        //   // select what fields to return - not very wise to return the hash
-        //   id: true,
-        //   email: true,
-        //   createdAt: true,
-        // },
-      });
-
-    // return new user
-    return user;
+          select: {
+            // select what fields to return - not very wise to return the hash
+            id: true,
+            email: true,
+            createdAt: true,
+          },
+        });
+      // return new user
+      return user;
+    } catch (error) {
+      if (
+        error instanceof
+        PrismaClientKnownRequestError
+      ) {
+        if (error.code === 'P2002') {
+          // duplicate email
+          throw new ForbiddenException(
+            'Credentials taken',
+          );
+        }
+      }
+      throw error;
+    }
   }
   signIn() {
     return { message: 'I am signed in' };
